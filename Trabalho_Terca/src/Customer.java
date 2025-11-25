60afa2e994c77cf6e8298fe4226b3479969c65b2
AndreyRP <119114347+AndreyRP@users.noreply.github.com>
2025-11-24 21:21:48 -0300

    Commit 3
    .

--- Trabalho_Terca/src/Customer.java
+++ Trabalho_Terca/src/Customer.java
@@ -47,23 +47,23 @@ public class Customer {
    }
 
 
-   private double amountFor(Rental each) {
-      //determine amounts for each line
+   private double amountFor(Rental aRental) {
+      //determine amounts for aRental line
       double var = 0;
-        switch (each.getMovie().getPriceCode()) {
+        switch (aRental.getMovie().getPriceCode()) {
         
            case Movie.REGULAR:
               var += 2;
-              if (each.getDaysRented() > 2)
-                 var += (each.getDaysRented() - 2) * 1.5;
+              if (aRental.getDaysRented() > 2)
+                 var += (aRental.getDaysRented() - 2) * 1.5;
               break;
            case Movie.NEW_RELEASE:
-              var += each.getDaysRented() * 3;
+              var += aRental.getDaysRented() * 3;
               break;
            case Movie.CHILDRENS:
               var += 1.5;
-              if (each.getDaysRented() > 3)
-                 var += (each.getDaysRented() - 3) * 1.5;
+              if (aRental.getDaysRented() > 3)
+                 var += (aRental.getDaysRented() - 3) * 1.5;
                break;
         }
 
