c03d4ae17f043bddf130a2bc51bc998a524452dd
AndreyRP <119114347+AndreyRP@users.noreply.github.com>
2025-11-24 22:02:50 -0300

    Commit 9
    .

--- Trabalho_Terca/src/Movie.java
+++ Trabalho_Terca/src/Movie.java
@@ -23,5 +23,28 @@ public class Movie {
     public String getTitle (){
         return _title;
     }
+
+    public double getCharge(int daysRented, Rental aRental){
+       //determine amounts for aRental line
+      double var = 0;
+        switch (aRental.getMovie().getPriceCode()) {
+        
+           case Movie.REGULAR:
+              var += 2;
+              if (aRental.getDaysRented() > 2)
+                 var += (aRental.getDaysRented() - 2) * 1.5;
+              break;
+           case Movie.NEW_RELEASE:
+              var += aRental.getDaysRented() * 3;
+              break;
+           case Movie.CHILDRENS:
+              var += 1.5;
+              if (aRental.getDaysRented() > 3)
+                 var += (aRental.getDaysRented() - 3) * 1.5;
+               break;
+        }
+
+        return var;
+   }
 }
 
--- Trabalho_Terca/src/Rental.java
+++ Trabalho_Terca/src/Rental.java
@@ -17,26 +17,7 @@ public class Rental {
     }
 
     double getCharge(Rental aRental) {
-      //determine amounts for aRental line
-      double var = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-        
-           case Movie.REGULAR:
-              var += 2;
-              if (aRental.getDaysRented() > 2)
-                 var += (aRental.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              var += aRental.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              var += 1.5;
-              if (aRental.getDaysRented() > 3)
-                 var += (aRental.getDaysRented() - 3) * 1.5;
-               break;
-        }
-
-        return var;
+      return _movie.getCharge(_daysRented, aRental);
    } 
 
    public int getFrequentRenterPoints(Rental each) { 
