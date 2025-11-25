d8715c2542f5865a3ba089bd6d6b2b4ca7d7b891
AndreyRP <119114347+AndreyRP@users.noreply.github.com>
2025-11-24 22:04:59 -0300

    Commit 10
    .

--- Trabalho_Terca/src/Movie.java
+++ Trabalho_Terca/src/Movie.java
@@ -46,5 +46,14 @@ public class Movie {
 
         return var;
    }
+
+   public int getFrequentRenterPoints(int daysRented, Rental each){
+       if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+            each.getDaysRented() > 1){
+                return 2;
+            }
+            return 1;
+   }
+
 }
 
--- Trabalho_Terca/src/Rental.java
+++ Trabalho_Terca/src/Rental.java
@@ -21,11 +21,7 @@ public class Rental {
    } 
 
    public int getFrequentRenterPoints(Rental each) { 
-        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-            each.getDaysRented() > 1){
-                return 2;
-            }
-            return 1;
+        return _movie.getFrequentRenterPoints(_daysRented, each);
    }
    
 }
