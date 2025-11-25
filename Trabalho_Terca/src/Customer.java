3ff914b63fe4dc837b05f4077af54b124d5054d2
AndreyRP <119114347+AndreyRP@users.noreply.github.com>
2025-11-24 21:24:51 -0300

    Commit 4
    .

--- Trabalho_Terca/src/Customer.java
+++ Trabalho_Terca/src/Customer.java
@@ -25,7 +25,7 @@ public class Customer {
      while (rentals.hasMoreElements()) {
         
         Rental each = (Rental) rentals.nextElement();
-        double thisAmount = amountFor(each);
+        double thisAmount = each.getCharge(each);
 
         // add frequent renter points
         frequentRenterPoints ++;
@@ -46,27 +46,4 @@ public class Customer {
      return result;
    }
 
-
-   private double amountFor(Rental aRental) {
-      //determine amounts for aRental line
-      double var = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-        
-           case Movie.REGULAR:
-              var += 2;
-              if (aRental.getDaysRented() > 2)
-                 var += (aRental.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              var += aRental.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              var += 1.5;
-              if (aRental.getDaysRented() > 3)
-                 var += (aRental.getDaysRented() - 3) * 1.5;
-               break;
-        }
-
-        return var;
-   } 
 }
--- Trabalho_Terca/src/Rental.java
+++ Trabalho_Terca/src/Rental.java
@@ -15,4 +15,27 @@ public class Rental {
     public Movie getMovie() {
         return _movie;
     }
+
+    double getCharge(Rental aRental) {
+      //determine amounts for aRental line
+      double var = 0;
+        switch (aRental.getMovie().getPriceCode()) {
+        
+           case Movie.REGULAR:
+              var += 2;
+              if (aRental.getDaysRented() > 2)
+                 var += (aRental.getDaysRented() - 2) * 1.5;
+              break;
+           case Movie.NEW_RELEASE:
+              var += aRental.getDaysRented() * 3;
+              break;
+           case Movie.CHILDRENS:
+              var += 1.5;
+              if (aRental.getDaysRented() > 3)
+                 var += (aRental.getDaysRented() - 3) * 1.5;
+               break;
+        }
+
+        return var;
+   } 
 }
