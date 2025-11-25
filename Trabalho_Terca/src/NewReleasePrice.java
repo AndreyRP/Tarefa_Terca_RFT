ba5586207a5853b17f6c0029d28a962ae645d740
AndreyRP <119114347+AndreyRP@users.noreply.github.com>
2025-11-25 19:42:37 -0300

    Commit 13
    .

--- Trabalho_Terca/src/NewReleasePrice.java
+++ Trabalho_Terca/src/NewReleasePrice.java
@@ -2,4 +2,8 @@ public class NewReleasePrice extends Price {
    public int getPriceCode() {
        return Movie.NEW_RELEASE;
    }
+    @Override
+   public double getCharge(int daysRented, Rental aRental){
+      return daysRented * 3;
+   } 
 }
